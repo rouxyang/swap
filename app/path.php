5b91c908b35bdc8ebf15e9237e45c4e4380f5aa9
Jingcheng Zhang <diogin@gmail.com>
2015-03-24 16:18:37 +0800

    w

--- app/path.php
+++ app/path.php
@@ -3,26 +3,20 @@
  * Web 应用程序各类组件所在的位置定义。可以任意安排各组件的位置。
  */
 # Web 应用根目录
-define('kern\root_dir',       DIRECTORY_SEPARATOR === '\\' ? str_replace('\\', '/', __DIR__) : __DIR__);
-# 框架核心目录
-define('kern\kern_dir',       kern\root_dir . '/../kern');
+define('kern\root_dir',    DIRECTORY_SEPARATOR === '\\' ? str_replace('\\', '/', __DIR__) : __DIR__);
+# 框架核心
+define('kern\kern_dir',    kern\root_dir . '/../kern');
 # 配置文件
-define('kern\config_dir',     kern\root_dir . '/config');
-# 控制器
-define('kern\controller_dir', kern\root_dir . '/controller');
+define('kern\config_dir',  kern\root_dir . '/config');
 # 相关数据
-define('kern\data_dir',       kern\root_dir . '/data');
-# 过滤器
-define('kern\filter_dir',     kern\root_dir . '/filter');
-# 控制器助手
-define('kern\helper_dir',     kern\root_dir . '/helper');
+define('kern\data_dir',    kern\root_dir . '/data');
 # 通用库
-define('kern\library_dir',    kern\root_dir . '/library');
-# 三方库
-define('kern\vendor_dir',     kern\root_dir . '/vendor');
+define('kern\library_dir', kern\root_dir . '/library');
 # 业务逻辑
-define('kern\logic_dir',      kern\root_dir . '/logic');
-# 视图文件
-define('kern\view_dir',       kern\root_dir . '/view');
-# Web 应用公共目录
-define('kern\web_dir',        kern\root_dir . '/web');
+define('kern\logic_dir',   kern\root_dir . '/logic');
+# 各个模块
+define('kern\module_dir',  kern\root_dir . '/module');
+# 三方库
+define('kern\vendor_dir',  kern\root_dir . '/vendor');
+# Web 应用入口
+define('kern\web_dir',     kern\root_dir . '/web');
